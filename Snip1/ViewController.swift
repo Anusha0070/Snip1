8784afed1d0b217678529a370e8eb6caa8845826
Anusha Raju <anusha.raju0070@gmail.com>
2024-11-11 09:42:52 -0800

    Initial Commit

---
+++ Snip1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Snip1
+//
+//  Created by Anusha Raju on 11/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Snip1Tests/Snip1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Snip1Tests.swift
+//  Snip1Tests
+//
+//  Created by Anusha Raju on 11/11/24.
+//
+
+import Testing
+@testable import Snip1
+
+struct Snip1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
